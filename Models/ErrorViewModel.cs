da37588a0d464171a9283778cf4a593f49d25bd0
Uvaancovie <uvaancovie@gmail.com>
2025-03-12 21:38:38 +0200

    staging

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ICE_TASK2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
